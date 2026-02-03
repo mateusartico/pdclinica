70a36ec9157cd5f158e392e1807cfc679441053a
igormartinz <igormartinssantos9020@gmail.com>
2026-02-03 16:33:38 -0300

    Revert "Merge pull request #1 from mateusartico/feature/autenticacao"
    This reverts commit a4fc8d521318df497f6b56397f79ddb714057a07, reversing
    changes made to fe7e6838f1f587adeecde6d009db115bbea170ff.

--- js/script.js
+++
@@ -1,7 +0,0 @@
-/* Checkbox Visual do reCAPTCHA */
-
-const checkbox = document.querySelector(".checkbox-recaptcha");
-
-checkbox.addEventListener("click", () => {
-  checkbox.classList.toggle("checked");
-});
